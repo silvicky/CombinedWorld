9790dfc4e31a6c9e09bc6d5860e53ee9ab39dbd6
Vicky Silviana <vickyisnotaboy@gmail.com>
2025-10-03 17:23:50 +0800

    optimize

--- src/main/java/io/silvicky/item/common/PublicFields.java
+++
@@ -1,8 +0,0 @@
-package io.silvicky.item.common;
-
-import net.minecraft.server.MinecraftServer;
-
-public class PublicFields
-{
-    public static MinecraftServer server;
-}
--- src/main/java/io/silvicky/item/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/io/silvicky/item/mixin/ServerPlayerInteractionManagerMixin.java
@@ -3,16 +3,12 @@ package io.silvicky.item.mixin;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.world.GameMode;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import static io.silvicky.item.common.Util.NETHER;
-import static io.silvicky.item.common.Util.OVERWORLD;
 
 @Mixin(ServerPlayerInteractionManager.class)
 public class ServerPlayerInteractionManagerMixin
