a9dc3d5dd5dd1640ec1429f21a2db1106f2d3090
Vicky Silviana <vickyisnotaboy@gmail.com>
2026-03-06 01:37:06 +0800

    fix other clients movement

--- src/main/java/io/silvicky/item/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/io/silvicky/item/mixin/ServerPlayNetworkHandlerMixin.java
@@ -3,11 +3,15 @@ package io.silvicky.item.mixin;
 import com.llamalad7.mixinextras.sugar.Local;
 import io.silvicky.item.backrooms.VecTransformer;
 import net.minecraft.entity.Entity;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -18,6 +22,10 @@ import static net.minecraft.util.math.MathHelper.floor;
 @Mixin(ServerPlayNetworkHandler.class)
 public abstract class ServerPlayNetworkHandlerMixin
 {
+
+    @Shadow protected abstract boolean shouldCheckMovement(boolean elytra);
+
+    @Shadow public ServerPlayerEntity player;
 
     @Redirect(method = "onPlayerInteractBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Vec3d;subtract(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;"))
     private Vec3d inject1(Vec3d instance, Vec3d vec)
@@ -34,5 +42,14 @@ public abstract class ServerPlayNetworkHandlerMixin
         {
             cir.setReturnValue(false);
         }
+    }
+    @Redirect(method = "onPlayerMove", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;shouldCheckMovement(Z)Z"))
+    private boolean inject3(ServerPlayNetworkHandler instance, boolean elytra, @Local(argsOnly = true) PlayerMoveC2SPacket packet)
+    {
+        if(!player.getChunkPos().equals(new ChunkPos(floor(packet.x)/16,floor(packet.z)/16)))
+        {
+            return false;
+        }
+        return shouldCheckMovement(elytra);
     }
 }
