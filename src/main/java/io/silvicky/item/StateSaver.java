a755f0ccf4b56b19f81553bce093d641f2c97f5a
Vicky Silviana <vickyisnotaboy@gmail.com>
2026-03-21 04:20:35 +0800

    silence

--- src/main/java/io/silvicky/item/StateSaver.java
+++ src/main/java/io/silvicky/item/StateSaver.java
@@ -40,6 +40,7 @@ public class StateSaver extends SavedData
     public final HashMap<Identifier, HashMap<String, ServerPlayer.RespawnConfig>> respawn;
     public final HashMap<Identifier, Integer> entityVisibility;
     public final HashMap<String, HashMap<String,Long>> playerVisibility;
+    public final HashMap<Identifier, Integer> silence;
     public static final Codec<Pair<ItemStack,Byte>> SLOT_CODEC=Codec.pair(ItemStack.CODEC.orElse(ItemStack.EMPTY),Codec.BYTE.fieldOf(SLOT).codec());
     private static final Codec<StateSaver> CODEC= RecordCodecBuilder.create((instance)->
             instance.group
@@ -69,7 +70,9 @@ public class StateSaver extends SavedData
                         Codec.unboundedMap(Identifier.CODEC, Codec.INT).xmap(HashMap::new, map->map).fieldOf("entity_visibility").orElse(new HashMap<>()).forGetter((stateSaver ->
                                 stateSaver.entityVisibility)),
                         Codec.unboundedMap(Codec.STRING, Codec.unboundedMap(Codec.STRING,Codec.LONG).xmap(HashMap::new,map->map)).xmap(HashMap::new,map->map).fieldOf("player_visibility").orElse(new HashMap<>()).forGetter((stateSaver)->
-                                stateSaver.playerVisibility)
+                                stateSaver.playerVisibility),
+                        Codec.unboundedMap(Identifier.CODEC, Codec.INT).xmap(HashMap::new, map->map).fieldOf("silence").orElse(new HashMap<>()).forGetter((stateSaver ->
+                                stateSaver.silence))
                     ).apply(instance,StateSaver::new));
     private StateSaver(LinkedList<StorageInfo> nbtList,
                       LinkedList<PositionInfo> posList,
@@ -83,7 +86,8 @@ public class StateSaver extends SavedData
                       HashMap<Identifier, LevelData.RespawnData> worldSpawn,
                       HashMap<Identifier, HashMap<String, ServerPlayer.RespawnConfig>> respawn,
                        HashMap<Identifier,Integer> entityVisibility,
-                       HashMap<String,HashMap<String,Long>> playerVisibility)
+                       HashMap<String,HashMap<String,Long>> playerVisibility,
+                       HashMap<Identifier,Integer> silence)
     {
         this.nbtList=nbtList;
         this.posList=posList;
@@ -98,11 +102,13 @@ public class StateSaver extends SavedData
         this.respawn=respawn;
         this.entityVisibility=entityVisibility;
         this.playerVisibility=playerVisibility;
+        this.silence=silence;
     }
     private StateSaver()
     {
         this(new LinkedList<>(),
                 new LinkedList<>(),
+                new HashMap<>(),
                 new HashMap<>(),
                 new HashMap<>(),
                 new HashMap<>(),
