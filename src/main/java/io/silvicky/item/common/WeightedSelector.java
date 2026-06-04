36edc981bdbc8597bcaff5ab43eae63265f2a3a2
Vicky Silviana <vickyisnotaboy@gmail.com>
2026-06-04 14:51:46 +0800

    WeightedSelector

---
+++ src/main/java/io/silvicky/item/common/WeightedSelector.java
@@ -0,0 +1,41 @@
+package io.silvicky.item.common;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
+
+public class WeightedSelector<T>
+{
+    private final Map<T,Integer> weights=new HashMap<>();
+    private final Random random=new Random();
+    private final TreeMap<Integer,T> borders=new TreeMap<>();
+    private void update()
+    {
+        borders.clear();
+        int cur=0;
+        for(Map.Entry<T,Integer> entry: weights.entrySet())
+        {
+            cur+=entry.getValue();
+            borders.put(cur, entry.getKey());
+        }
+    }
+    public void put(T key, int value)
+    {
+        if(value<=0)
+        {
+            weights.remove(key);
+        }
+        else
+        {
+            weights.put(key,value);
+        }
+        update();
+    }
+    public T select()
+    {
+        int key= random.nextInt(65536);
+        Map.Entry<Integer,T> entry=borders.higherEntry(key);
+        return entry==null?null:entry.getValue();
+    }
+}
