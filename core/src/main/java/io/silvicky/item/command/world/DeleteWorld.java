90b9cb7817dffc96c0e30f7b84f0054299c0e225
Vicky Silviana <vickyisnotaboy@gmail.com>
2026-07-26 18:33:45 +0800

    move time-consuming tasks into independent thread & bugfix new dimensions not deleted (evacuate cannot be moved now)

--- core/src/main/java/io/silvicky/item/command/world/DeleteWorld.java
+++ core/src/main/java/io/silvicky/item/command/world/DeleteWorld.java
@@ -73,94 +73,122 @@ public class DeleteWorld {
             source.sendSuccess(()-> Component.literal("Hello, admin! This command can delete a world. The world will be lost forever(a long time). It is still strongly suggested that you backup your save first. Also you need to read the result carefully. Type this command without arguments to see the help. Type this command again if you already understand what you are doing."),false);
             return Command.SINGLE_SUCCESS;
         }
-        id=getDimensionId(idTmp);
-        if(id.getNamespace().equals("minecraft"))
+        source.sendSuccess(()-> Component.literal("Running..."),false);
+        id = getDimensionId(idTmp);
+        if (id.getNamespace().equals("minecraft"))
         {
-            source.sendSuccess(()-> Component.literal("how dare you..."),false);
+            source.sendSuccess(() -> Component.literal("how dare you..."), false);
             return Command.SINGLE_SUCCESS;
         }
-        final boolean isSinglet= !id.getPath().endsWith(OVERWORLD);
-        Identifier idNether=null;
-        Identifier idEnd=null;
-        if(!isSinglet)
+        final boolean isSinglet = !id.getPath().endsWith(OVERWORLD);
+        Identifier idNether;
+        Identifier idEnd;
+        if (!isSinglet)
         {
             String tmp1 = id.getPath().substring(0, id.getPath().length() - OVERWORLD.length());
             idNether = Identifier.fromNamespaceAndPath(id.getNamespace(), tmp1 + NETHER);
             idEnd = Identifier.fromNamespaceAndPath(id.getNamespace(), tmp1 + END);
         }
-        MinecraftServer server=source.getServer();
-        StateSaver stateSaver = StateSaver.getServerState(server);
-        ServerLevel src=server.getLevel(ResourceKey.create(Registries.DIMENSION,id));
-        if(src!=null)
+        else
         {
-            if(getDimensionId(id).equals(getDimensionId(safeZone)))
+            idEnd = null;
+            idNether = null;
+        }
+        MinecraftServer server = source.getServer();
+        StateSaver stateSaver = StateSaver.getServerState(server);
+        ServerLevel src = server.getLevel(ResourceKey.create(Registries.DIMENSION, id));
+        if (src != null)
+        {
+            deletedDimensions.add(ResourceKey.create(Registries.LEVEL_STEM, id));
+            if (!isSinglet)
             {
-                source.sendSuccess(()-> Component.literal("Target dimension cannot be the same as the one to be deleted."),false);
+                deletedDimensions.add(ResourceKey.create(Registries.LEVEL_STEM, idNether));
+                deletedDimensions.add(ResourceKey.create(Registries.LEVEL_STEM, idEnd));
+            }
+            source.sendSuccess(() -> Component.literal("Dimension options deleted."), false);
+            if (getDimensionId(id).equals(getDimensionId(safeZone)))
+            {
+                source.sendSuccess(() -> Component.literal("Target dimension cannot be the same as the one to be deleted."), false);
                 return Command.SINGLE_SUCCESS;
             }
-            banWarp(source,src,StateSaver.WarpRestrictionInfo.INFINITE,"To be deleted.",true);
-            evacuate(source,src,safeZone,false);
-            deletedDimensions.add(ResourceKey.create(Registries.LEVEL_STEM,id));
-            if(!isSinglet)
-            {
-                deletedDimensions.add(ResourceKey.create(Registries.LEVEL_STEM,idNether));
-                deletedDimensions.add(ResourceKey.create(Registries.LEVEL_STEM,idEnd));
-            }
-            source.sendSuccess(()-> Component.literal("Dimension options deleted."),false);
-            source.sendSuccess(()-> Component.literal("First stage is done. Restart the game and type the same command again to continue."),false);
-            if(!source.getServer().isDedicatedServer())source.sendSuccess(()-> Component.literal("DO NOT ENTER THIS WORLD AGAIN BEFORE RESTARTING YOUR GAME OR YOUR SAVE WOULD BE DESTROYED!!!"),false);
+            banWarp(source, src, StateSaver.WarpRestrictionInfo.INFINITE, "To be deleted.", true);
+            evacuate(source, src, safeZone, false);
+            source.sendSuccess(() -> Component.literal("First stage is done. Restart the game and type the same command again to continue."), false);
+            if (!source.getServer().isDedicatedServer())
+                source.sendSuccess(() -> Component.literal("DO NOT ENTER THIS WORLD AGAIN BEFORE RESTARTING YOUR GAME OR YOUR SAVE WOULD BE DESTROYED!!!"), false);
             return Command.SINGLE_SUCCESS;
         }
-        try
+        new Thread(()->
         {
-            stateSaver.seed.remove(id);
-            stateSaver.restrictionInfoHashMap.remove(id);
-            if (!isSinglet) {
-                stateSaver.seed.remove(idNether);
-                stateSaver.seed.remove(idEnd);
-                stateSaver.restrictionInfoHashMap.remove(idNether);
-                stateSaver.restrictionInfoHashMap.remove(idEnd);
-            }
-            source.sendSuccess(() -> Component.literal("Seeds deleted."), false);
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-            source.sendSuccess(()-> Component.literal("ERR: Failed to delete seed! Please manually delete them."),false);
-        }
-        try
-        {
-            Set<String> removedPlayers = stateSaver.posMap.getOrDefault(id,new HashMap<>()).keySet();
-            int cnt = removedPlayers.size();
-            for(String i:removedPlayers)stateSaver.savedMap.getOrDefault(id.getNamespace(),new HashMap<>()).remove(i);
-            source.sendSuccess(() -> Component.literal("Deleted " + cnt + " player data."), false);
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-            source.sendSuccess(()-> Component.literal("ERR: Failed to delete player data! Please manually delete them."),false);
-        }
-        try
-        {
-            Path target = server.getWorldPath(LevelResource.ROOT).resolve("dimensions").resolve(id.getNamespace());
-            Path targetOverworld=target.resolve(id.getPath());
-            deleteFolder(targetOverworld);
-            if(!isSinglet)
+            try
             {
-                Path targetNether=target.resolve(idNether.getPath());
-                Path targetEnd=target.resolve(idEnd.getPath());
-                deleteFolder(targetNether);
-                deleteFolder(targetEnd);
+                if(newDimensions.containsKey(ResourceKey.create(Registries.LEVEL_STEM, id)))
+                {
+                    newDimensions.remove(ResourceKey.create(Registries.LEVEL_STEM, id));
+                    if (!isSinglet)
+                    {
+                        newDimensions.remove(ResourceKey.create(Registries.LEVEL_STEM, idNether));
+                        newDimensions.remove(ResourceKey.create(Registries.LEVEL_STEM, idEnd));
+                    }
+                }
+                try
+                {
+                    stateSaver.seed.remove(id);
+                    stateSaver.restrictionInfoHashMap.remove(id);
+                    if (!isSinglet)
+                    {
+                        stateSaver.seed.remove(idNether);
+                        stateSaver.seed.remove(idEnd);
+                        stateSaver.restrictionInfoHashMap.remove(idNether);
+                        stateSaver.restrictionInfoHashMap.remove(idEnd);
+                    }
+                    source.sendSuccess(() -> Component.literal("Seeds deleted."), false);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                    source.sendSuccess(() -> Component.literal("ERR: Failed to delete seed! Please manually delete them."), false);
+                }
+                try
+                {
+                    Set<String> removedPlayers = stateSaver.posMap.getOrDefault(id, new HashMap<>()).keySet();
+                    int cnt = removedPlayers.size();
+                    for (String i : removedPlayers)
+                        stateSaver.savedMap.getOrDefault(id.getNamespace(), new HashMap<>()).remove(i);
+                    source.sendSuccess(() -> Component.literal("Deleted " + cnt + " player data."), false);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                    source.sendSuccess(() -> Component.literal("ERR: Failed to delete player data! Please manually delete them."), false);
+                }
+                try
+                {
+                    Path target = server.getWorldPath(LevelResource.ROOT).resolve("dimensions").resolve(id.getNamespace());
+                    Path targetOverworld = target.resolve(id.getPath());
+                    deleteFolder(targetOverworld);
+                    if (!isSinglet)
+                    {
+                        Path targetNether = target.resolve(idNether.getPath());
+                        Path targetEnd = target.resolve(idEnd.getPath());
+                        deleteFolder(targetNether);
+                        deleteFolder(targetEnd);
+                    }
+                    target.toFile().delete();
+                    source.sendSuccess(() -> Component.literal("Deleted save files."), false);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                    source.sendSuccess(() -> Component.literal("ERR: Failed to delete save files! Please manually delete them."), false);
+                }
+                source.sendSuccess(() -> Component.literal("Done."), false);
             }
-            target.toFile().delete();
-            source.sendSuccess(()-> Component.literal("Deleted save files."),false);
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-            source.sendSuccess(()-> Component.literal("ERR: Failed to delete save files! Please manually delete them."),false);
-        }
-        source.sendSuccess(()-> Component.literal("Done."),false);
+            catch (Exception e)
+            {
+                source.sendFailure(Component.literal(e.toString()));
+            }
+        }).start();
         return Command.SINGLE_SUCCESS;
     }
 }
