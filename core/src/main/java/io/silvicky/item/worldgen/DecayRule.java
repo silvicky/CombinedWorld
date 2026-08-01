8e2bd349ff681ed75d5f3ee3130892bc2abf730b
Vicky Silviana <vickyisnotaboy@gmail.com>
2026-08-01 22:40:14 +0800

    fix decay random

--- core/src/main/java/io/silvicky/item/worldgen/DecayRule.java
+++ core/src/main/java/io/silvicky/item/worldgen/DecayRule.java
@@ -1,10 +1,11 @@
 package io.silvicky.item.worldgen;
 
 import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.levelgen.RandomState;
 import org.jspecify.annotations.NonNull;
 
 public interface DecayRule
 {
-    boolean decay(@NonNull ChunkAccess chunk);
+    boolean decay(@NonNull ChunkAccess chunk, @NonNull RandomState randomState);
     String name();
 }
