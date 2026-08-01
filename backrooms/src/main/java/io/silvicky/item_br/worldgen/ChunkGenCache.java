38fdeb09dfdb1c248bab9efdfea364758da67615
Vicky Silviana <vickyisnotaboy@gmail.com>
2026-08-01 23:26:14 +0800

    use unique caches

--- backrooms/src/main/java/io/silvicky/item_br/worldgen/ChunkGenCache.java
+++ backrooms/src/main/java/io/silvicky/item_br/worldgen/ChunkGenCache.java
@@ -2,6 +2,7 @@ package io.silvicky.item_br.worldgen;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.Identifier;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.block.Blocks;
@@ -23,6 +24,10 @@ public class ChunkGenCache
 
     private final int height;
 
+    private final ServerLevel level;
+
+    private final RandomState randomState;
+
     private final Map<ChunkPos, SimpleChunk> chunks=new HashMap<>();
 
     private static ChunkPos shift(ChunkPos pos,int x,int z)
@@ -30,10 +35,12 @@ public class ChunkGenCache
         return new ChunkPos(pos.x()+x,pos.z()+z);
     }
 
-    public ChunkGenCache(int baseY, int height)
+    public ChunkGenCache(int baseY, int height, ServerLevel level, RandomState randomState)
     {
         this.baseY = baseY;
         this.height = height;
+        this.level = level;
+        this.randomState = randomState;
     }
 
     public void setBlockState(BlockPos pos, BlockState state)
@@ -47,32 +54,32 @@ public class ChunkGenCache
         }
     }
 
-    private BlockPos getChosenPos(ChunkPos chunkPos, RandomState randomState)
+    private BlockPos getChosenPos(ChunkPos chunkPos)
     {
         RandomSource random=randomState.getOrCreateRandomFactory(key).at(chunkPos.x(),0,chunkPos.z());
         return chunkPos.getBlockAt(random.nextInt(3,13),0,random.nextInt(3,13));
     }
 
-    private void requestChunk(ChunkPos chunkPos, RandomState randomState)
+    private void requestChunk(ChunkPos chunkPos)
     {
         //TODO draw real things
         int[][] neighbors={{1,0},{0,1}};
-        BlockPos core=getChosenPos(chunkPos, randomState);
+        BlockPos core=getChosenPos(chunkPos);
         boolean[] coordination=getNodeCoordination(randomState,chunkPos.x(),chunkPos.z());
         for (int i=0;i<2;i++)
         {
-            BlockPos cur = getChosenPos(shift(chunkPos, neighbors[i][0], neighbors[i][1]), randomState);
+            BlockPos cur = getChosenPos(shift(chunkPos, neighbors[i][0], neighbors[i][1]));
             if(coordination[i])drawLine(core.getX(), core.getZ(), cur.getX(), cur.getZ(), (x, z) -> setBlockState(new BlockPos(x, 0, z), Blocks.CONCRETE.white().defaultBlockState()));
         }
     }
 
-    public void apply(ChunkAccess chunk, RandomState randomState)
+    public void apply(ChunkAccess chunk)
     {
         //TODO Dismiss generated chunks
         ChunkPos chunkPos=chunk.getPos();
         for(int x=-1;x<=0;x++)
             for(int z=-1;z<=0;z++)
-                requestChunk(shift(chunkPos,x,z), randomState);
+                requestChunk(shift(chunkPos,x,z));
         SimpleChunk simpleChunk=chunks.get(chunkPos);
         if(simpleChunk!=null)
         {
--- backrooms/src/main/java/io/silvicky/item_br/worldgen/Road2CustomRule.java
+++ backrooms/src/main/java/io/silvicky/item_br/worldgen/Road2CustomRule.java
@@ -1,18 +1,25 @@
 package io.silvicky.item_br.worldgen;
 
 import io.silvicky.item.worldgen.CustomRule;
+import net.minecraft.resources.Identifier;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.levelgen.RandomState;
 import org.jspecify.annotations.NonNull;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Road2CustomRule implements CustomRule
 {
-    private final ChunkGenCache chunkGenCache=new ChunkGenCache(0,256);
+    private final Map<Identifier, ChunkGenCache> caches = new HashMap<>();
 
     @Override
     public void gen(@NonNull ChunkAccess chunk, @NonNull RandomState randomState)
     {
-        chunkGenCache.apply(chunk,randomState);
+        ServerLevel level= (ServerLevel)(chunk.levelHeightAccessor);
+        ChunkGenCache cache=caches.computeIfAbsent(level.dimension().identifier(),_->new ChunkGenCache(0,256,level,randomState));
+        cache.apply(chunk);
     }
 
     @Override
