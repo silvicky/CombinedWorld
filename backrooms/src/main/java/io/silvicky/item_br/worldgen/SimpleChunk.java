5ee4ab8f697ff579f64d1abafde95ffeb7dd0799
Vicky Silviana <vickyisnotaboy@gmail.com>
2026-09-01 23:56:49 +0800

    use Map inside SimpleChunk

--- backrooms/src/main/java/io/silvicky/item_br/worldgen/SimpleChunk.java
+++ backrooms/src/main/java/io/silvicky/item_br/worldgen/SimpleChunk.java
@@ -5,6 +5,9 @@ import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SimpleChunk
 {
     private final int baseY;
@@ -13,44 +16,33 @@ public class SimpleChunk
 
     private final ChunkPos chunkPos;
 
-    private final BlockState[][][] blockStates;
+    private final Map<BlockPos, BlockState> blockStates=new HashMap<>();
 
     public SimpleChunk(int baseY, int height, ChunkPos chunkPos)
     {
         this.baseY = baseY;
         this.height = height;
         this.chunkPos = chunkPos;
-        this.blockStates=new BlockState[height][16][16];
     }
 
     public void setBlockState(BlockPos pos, BlockState state)
     {
         if(chunkPos.contains(pos)&&pos.getY()>=this.baseY&&pos.getY()<this.baseY+height)
         {
-            blockStates[pos.getY() - baseY]
-                    [pos.getX() - chunkPos.getMinBlockX()]
-                    [pos.getZ() - chunkPos.getMinBlockZ()]
-                    = state;
+            blockStates.put(pos, state);
         }
     }
 
     public BlockState getBlockState(BlockPos pos)
     {
-        if(chunkPos.contains(pos)&&pos.getY()>=this.baseY&&pos.getY()<this.baseY+height)
-        {
-            return blockStates[pos.getY() - baseY]
-                    [pos.getX() - chunkPos.getMinBlockX()]
-                    [pos.getZ() - chunkPos.getMinBlockZ()];
-        }
-        return null;
+        return blockStates.get(pos);
     }
 
     public void apply(ChunkAccess chunk)
     {
-        for(int i=0;i<height;i++)for(int x = 0; x <16; x++)for(int z = 0; z <16; z++)
+        for(Map.Entry<BlockPos, BlockState> entry:blockStates.entrySet())
         {
-            BlockState state=blockStates[i][x][z];
-            if(state!=null)chunk.setBlockState(chunkPos.getBlockAt(x,i+baseY,z),state);
+            chunk.setBlockState(entry.getKey(), entry.getValue());
         }
     }
 }
